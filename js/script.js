59f2d95db28f20968e00c4ec7d87c090e7529fab
Timas <timas2393@gmail.com>
2024-04-23 15:04:13 +0300

    add .trim()

--- js/script.js
+++ js/script.js
@@ -101,8 +101,8 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", ''),
-              b = prompt("На сколько оцените его?", '');
+        const a = prompt("Один из последних просмотренных фильмов?", '').trim(),
+              b = prompt("На сколько оцените его?", '').trim();
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log('done');
